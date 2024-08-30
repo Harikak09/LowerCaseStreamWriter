404e566799e9062ed641d9d114d223324d5a22e8
Harikak09 <harikakarumudi04@gmail.com>
2024-08-30 23:41:47 +0530

    All files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/***************************************
+ * 
+ * FileName     : Program.cs
+ * 
+ * Author       : Karumudi Harika
+ * 
+ * Project      : HelloWorld
+ * 
+ * Description  : String passed should be written to the stream in lower case.
+ * 
+ * *************************************/
+
+/// <summary>
+/// A LowerStreamWriter class is created which inherits StreamWriter.
+/// </summary>
+/// <returns>A lower case stream</returns>
+public class LowerStreamWriter : StreamWriter
+{
+    //A constructor which passes the stream of type Stream to its base class
+    public LowerStreamWriter(Stream stream) :
+        base(stream){ }
+
+    /// <summary>
+    /// A function to convert the string into lower case string.
+    /// </summary>
+    /// <param name="value"></param>
+    public override void WriteLine(string value)
+    {
+        //Coverts to lower case.
+        base.WriteLine(value.ToLower());
+    }
+
+}
+public class Program
+{
+    /// <summary>
+    /// Main program to create a instance of LowerStreamWriter which is a subclass of StreamWriter and StreamWriter is subclass of TextWriter.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new LowerStreamWriter(Console.OpenStandardOutput());
+
+        textWriter.WriteLine("Hello, World!");
+
+        textWriter.Flush();
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,28 @@
+# Overview
+Code inside a console application:
+
+static void Main(string[] args)
+{
+    TextWriter textWriter = new StreamWriter(Console.OpenStandardOutput());
+
+    textWriter.WriteLine("Hello, World!");
+
+    textWriter.Flush();
+}
+
+# Task
+Modify this code such that any string passed should be written to the Stream in lower case. So the output in this case should instead read: **hello, world!**
+
+# Do you see inheritance and polymorphism?
+Yes, there is inheritence with LowerStreamWriter, StreamWriter, TextWriter(Abstract Class in .NET) and polymorphism in function WriteLine. 
+
+# Design
+Classes - TextWriter(Abstract Class), StreamWriter(Inherits TextWriter), LowerStreamWriter(Inherits StreamWriter).
+
+All three contains WriteLine but in subclasses it overrides the same function of the base class.
+![Class Diagram]()
+
+
+
+
+
